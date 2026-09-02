acec8cd9337be6c3ffff09cafaa691e847b68abe
MichaelArmstrong1123 <fishforbrick@gmail.com>
2026-09-02 13:40:07 +0000

    add portfolio page to website

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(255, 253, 253)"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,22 +27,32 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+    //createPlatform(x, y, width, height, "color")
+createPlatform(700, 500, 150, 25, "whitesmoke")
+createPlatform(440, 620, 150, 25, "whitesmoke")
+createPlatform(950, 400, 150, 25, "")
+createPlatform(1200, 300, 150, 25, "")
+createPlatform(900, 200, 150, 25, "")
+createPlatform(600, 200, 150, 25, "")
     // TODO 3 - Create Collectables
+createCollectable("kennedi", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
+createCollectable("kennedi", 1000, 150);
 
 
 
     
     // TODO 4 - Create Cannons
-
-
+createCannon("top", 200, 100);
+createCannon("right", 300, 2000);
+createCannon("right", 200, 2000);
+createCannon("right", 700, 100);
+createCannon("top", 600, 1000);
+createCannon("top", 900, 1000);
     
     
     //////////////////////////////////
